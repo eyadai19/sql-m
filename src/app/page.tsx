b7f22ae11686f142666e386e9a79b26601714e16
eyadai19 <eyad.sy19@icloud.com>
2024-10-27 14:32:40 +0300

    login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+	// e
 	return (
 		<div className="p-4 text-base text-red-600 md:text-lg lg:text-xl">
 			<Button>click here</Button>
