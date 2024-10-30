eac279a36596203619940efd20bf4a7837be1c55
eyadai19 <eyad.sy19@icloud.com>
2024-10-30 19:18:05 +0300

    last

--- src/lib/types/userSchema.ts
+++ src/lib/types/userSchema.ts
@@ -1,7 +1,7 @@
+import { createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
-import { BaseZodError } from "./errorUtilities";
-import { createSelectSchema } from 'drizzle-zod';
 import { TB_user } from "../schema";
+import { BaseZodError } from "./errorUtilities";
 
 export const loginFormSchema = z.object({
 	username: z.string().min(8),
@@ -16,14 +16,10 @@ export const registerFormSchema = z.object({
 export type LoginFormError = BaseZodError<typeof loginFormSchema>;
 export type RegisterFormError = BaseZodError<typeof registerFormSchema>;
 
-
-
-
 export const userSchema = createSelectSchema(TB_user, {
 	username: (schema) => schema.username.min(6).max(24),
 	password: (schema) => schema.password.min(8).max(32),
 });
-
 
 export const userViewSchema = userSchema.pick({
 	id: true,
@@ -33,4 +29,3 @@ export const userViewSchema = userSchema.pick({
 export type UserView = z.infer<typeof userViewSchema>;
 
 export type User = z.infer<typeof userSchema>;
-
