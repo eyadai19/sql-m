160df1a7b8e4b3130f99855a94667c0a72aad268
eyadai19 <eyad.sy19@icloud.com>
2024-11-06 12:11:41 +0300

    last_merge

--- src/app/(main)/DML/select/page.tsx
+++ src/app/(main)/DML/select/page.tsx
@@ -5,6 +5,12 @@ as
 into
 */
 
+import Excercise from "@/components/Excercise";
+
 export default function select() {
-	return <div className="p-4 text-base md:text-lg lg:text-xl"></div>;
+	return (
+		<div className="p-4 text-base md:text-lg lg:text-xl">
+			<Excercise />
+		</div>
+	);
 }
