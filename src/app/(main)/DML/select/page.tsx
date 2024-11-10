fb06ac25d19c0c6ff006c50d81e18f91e9fc0425
eyadai19 <eyad.sy19@icloud.com>
2024-11-10 20:05:49 +0300

    select-page-param

--- src/app/(main)/DML/select/page.tsx
+++ src/app/(main)/DML/select/page.tsx
@@ -4,19 +4,11 @@ select
 as
 into
 */
-import CodeBlock from "@/components/CodeBlock";
-import Explanation from "@/components/Explanation";
-import SelectPage from "@/components/SelectPage"
+import SelectPage from "@/components/pages/DML/SelectPage";
 export default function select() {
-
-	
 	return (
 		<div className="p-4 text-base md:text-lg lg:text-xl">
 			<SelectPage />
-		
-			
-
-
 		</div>
 	);
 }
