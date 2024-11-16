9214d4960a8b0cdcf9bc43ce32d369a55200230a
eyadai19 <eyad.sy19@icloud.com>
2024-11-16 20:58:01 +0300

    j

--- src/lib/ServerAction/chatBotNLP.tsx
+++ src/lib/ServerAction/chatBotNLP.tsx
@@ -1,10 +1,9 @@
-"use server";
+// "use server";
 import axios from "axios";
 import { z } from "zod";
 import {
 	userChatBotInputSchema,
 	userExcerciseAnswerError,
-	userExcerciseAnswerSchema,
 } from "../types/userSchema";
 
 export async function ChatbotAction(
@@ -12,11 +11,37 @@ export async function ChatbotAction(
 ): Promise<{ answer: string } | userExcerciseAnswerError | undefined> {
 	"use server";
 	try {
-		const question = await userExcerciseAnswerSchema.parseAsync({ input });
+		const question = await userChatBotInputSchema.parseAsync({ input });
 		const response = await axios.post("http://localhost:3000/chatbot", {
 			question,
 		});
 		return { answer: response.data.answer };
+	} catch (error) {
+		console.error("Error sending question to API:", error);
+		return { field: "root", message: "Failed to retrieve answer" };
+	}
+}
+
+export async function ChatbotExpAction(
+	input: z.infer<typeof userChatBotInputSchema>,
+): Promise<
+	| { answer: String }
+	| { question: String; answers: String[] }
+	| userExcerciseAnswerError
+	| undefined
+> {
+	"use server";
+	try {
+		const choose = await userChatBotInputSchema.parseAsync({ input });
+		const response = await axios.post("http://localhost:3000/chatbot", {
+			choose,
+		});
+		if (response.data) return { answer: response.data.answer };
+		else {
+			const a = ["d", "d"]; // get the answers array from response json
+			// const asnwers = a.map((x) => ({ asnwer: x }));
+			return { question: response.data.asnw, answers: a };
+		}
 	} catch (error) {
 		console.error("Error sending question to API:", error);
 		return { field: "root", message: "Failed to retrieve answer" };
