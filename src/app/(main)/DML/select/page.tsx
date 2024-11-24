50dade5311560b45150630d54a1cf7d8a87b804d
eyadai19 <eyad.sy19@icloud.com>
2024-11-24 10:45:41 +0300

    ....

--- src/app/(main)/DML/select/page.tsx
+++ src/app/(main)/DML/select/page.tsx
@@ -6,11 +6,17 @@ into
 */
 import Select from "@/components/pages/DML/select";
 export default function select() {
+<<<<<<< Updated upstream
 
 	
 	return (
 		<div className="p-4 text-base md:text-lg lg:text-xl">
 			<Select />
+=======
+	return (
+		<div className="p-4 text-base md:text-lg lg:text-xl">
+			<Excercise/>
+>>>>>>> Stashed changes
 		</div>
 	);
 }
