abd8a497f55245f5790a1db60c4eab4296081cab
zaidNa03 <alnahhaszaed27@gmail.com>
2025-01-10 17:39:42 +0300

    removed exercise from basic

--- src/components/pages/basic/reference.tsx
+++ src/components/pages/basic/reference.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import DragDropExercise from "@/components/Exercise/DragDropExercise/DragDropExercise";
-import Exercise from "@/components/Exercise/Exercise";
 import MultipleChoiceExercise from "@/components/Exercise/MultipleChoiceExercise/MultipleChoiceExercise";
 import TrueFalseExercise from "@/components/Exercise/TrueFalseExercise/TrueFalseExercise";
 import Explanation from "@/components/Explanation/Explanation";
 import { Button } from "@/components/ui/button";
-import { UserExcerciseAnswerAction } from "@/lib/ServerAction/userExcerciseAnswerAction";
 import PAGE_DATA from "@/utils/pagesData";
 import { pageName } from "@/utils/path";
 import Link from "next/link";
@@ -53,10 +51,11 @@ export default function Reference({
 
 	if (hasAccess === undefined) {
 		return (
-				<div className="flex h-screen items-center justify-center">
-					<div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-[#ADF0D1]"></div>
-				</div>
-			);}
+			<div className="flex h-screen items-center justify-center">
+				<div className="h-16 w-16 animate-spin rounded-full border-b-4 border-t-4 border-[#ADF0D1]"></div>
+			</div>
+		);
+	}
 
 	return (
 		<div>
@@ -65,13 +64,13 @@ export default function Reference({
 			<DragDropExercise {...dragDropParams} />
 			<MultipleChoiceExercise {...multipleChoiceParams} />
 			<TrueFalseExercise {...trueFalseParams} />
-			<Exercise
+			{/*}<Exercise
 				{...exerciseParams}
 				UserExcerciseAnswerAction={UserExcerciseAnswerAction.bind(
 					null,
 					pageName.reference,
 				)}
-			/>
+			/>{*/}
 		</div>
 	);
 }
